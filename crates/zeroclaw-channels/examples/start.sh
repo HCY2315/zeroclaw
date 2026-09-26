391f5bae46f3167650c8f931cb7b965debe8662c
bingchengtuo <bingchengtuo@163.com>
2026-09-26 21:17:34 +0800

    飞书1

---
+++ crates/zeroclaw-channels/examples/start.sh
@@ -0,0 +1,5 @@
+#cargo build --release -p zeroclaw-channels --features channel-lark --example feishu_echo
+#cp crates/zeroclaw-channels/examples/feishu.toml ./
+##./target/release/examples/feishu_echo
+
+cargo run --package zeroclaw-channels --example feishu_echo
